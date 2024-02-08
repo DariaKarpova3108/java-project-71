a9156eed00efa2e0a6f7e9e2011d0f5aa01048ff
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-02-08 23:19:37 +0300

    update class App and added class Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.Arrays;
+
+@CommandLine.Command
+public class App implements Runnable {
+    @CommandLine.Option(names = {"-f", "--format=format"}, paramLabel = "format", defaultValue = "", description = "output format [default: stylish]")
+    private String format;
+
+    @CommandLine.Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private String help;
+
+    @CommandLine.Option(names = {"-v", "--version"}, description = "Print version information and exit.")
+    private String version;
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+
+    public void run() {
+        System.out.println(filepath1 + " path to first file");
+        System.out.println(filepath2 + " path to second file");
+        System.out.println(format + " output format [default: stylish]");
+        System.out.println(help + " Show this help message and exit.");
+        System.out.println(version + " Print version information and exit.");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Arrays.toString(args));
+        new CommandLine(new App()).execute(args);
     }
 }
