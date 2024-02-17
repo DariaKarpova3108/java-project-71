89cfbc4d6bf790165abbec76c809f3a8a9996146
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-02-18 02:30:30 +0300

    update classes App and Differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,70 +4,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "", description = "Compares two configuration files and shows a difference.")
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<String> {
-
+public class App implements Callable<Integer> {
+    final int SUCCESSFUL_EXIT = 0;
+    final int WRONG_OUTPUT = 1;
     private static ObjectMapper mapper = new ObjectMapper();
     @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format;
-
     @CommandLine.Parameters(index = "0", description = "path to first file")
-    private String filepath1 = "/Users/dariakarpova/Documents/java-project-71/app/filepath1.json";
-
+    private String filepath1 = "path first";
     @CommandLine.Parameters(index = "1", description = "path to second file")
-    private String filepath2 = "/Users/dariakarpova/Documents/java-project-71/app/filepath2.json";
+    private String filepath2 = "path second";
 
     @Override
-    public String call() {
-        Map<String, Object> file1 = null;
-        try {
-            file1 = readAndConvertFile1(filepath1);
-        } catch (IOException e) {
-            System.out.println("Error reading and converting file1: " + e.getMessage());
-        }
-        Map<String, Object> file2 = null;
-        try {
-            file2 = readAndConvertFile2(filepath2);
-        } catch (IOException e) {
-            System.out.println("Error reading and converting file2: " + e.getMessage());
-        }
+    public Integer call() throws IOException {
 
-        Differ gif = new Differ();
+        Map<String, Object> file1 = Differ.readAndConvertFile1();
+        Map<String, Object> file2 = Differ.readAndConvertFile2();
 
         try {
-            String result = gif.generate(file1, file2);
+            String result = Differ.generate(file1, file2);
             System.out.println(result);
-            return result;
+            return SUCCESSFUL_EXIT;
         } catch (Exception ex) {
             System.out.println("Error reading files: " + ex.getMessage());
-            return "";
+            return WRONG_OUTPUT;
         }
-    }
-
-    public static Map<String, Object> readAndConvertFile1(String filepath) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(filepath)));
-        ContentFile1 readerFile1 = mapper.readValue(content, ContentFile1.class);
-        return mapper.convertValue(readerFile1, Map.class);
-    }
-
-    public static Map<String, Object> readAndConvertFile2(String filepath) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(filepath)));
-        ContentFile2 readerFile2 = mapper.readValue(content, ContentFile2.class);
-        return mapper.convertValue(readerFile2, Map.class);
-
     }
 
     public static void main(String[] args) {
         App app = new App();
+
         CommandLine commandLine = new CommandLine(app);
         commandLine.execute(args);
     }
 }
+
 
 
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,31 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Differ {
-    public String generate(Map<String, Object> file1, Map<String, Object> file2) {
-        String list = "{" + "\n";
+    static ObjectMapper mapper = new ObjectMapper();
+
+    public static Map<String, Object> readAndConvertFile1() throws IOException {
+        String filepath1 = "/Users/dariakarpova/Documents/java-project-71/app/filepath1.json";
+        String content = new String(Files.readAllBytes(Paths.get(filepath1)));
+        ContentFile1 readerFile1 = mapper.readValue(content, ContentFile1.class);
+        return mapper.convertValue(readerFile1, Map.class);
+    }
+
+    public static Map<String, Object> readAndConvertFile2() throws IOException {
+        String filepath2 = "/Users/dariakarpova/Documents/java-project-71/app/filepath2.json";
+        String content = new String(Files.readAllBytes(Paths.get(filepath2)));
+        ContentFile2 readerFile2 = mapper.readValue(content, ContentFile2.class);
+        return mapper.convertValue(readerFile2, Map.class);
+    }
+
+    public static String generate(Map<String, Object> file1, Map<String, Object> file2) {
+        String list = "";
         for (Map.Entry<String, Object> entry : file1.entrySet()) {
             Object value = entry.getValue();
             String key = entry.getKey();
@@ -24,10 +45,28 @@ public class Differ {
             String key2 = entry2.getKey();
             Object value2 = entry2.getValue();
             if (!file1.containsKey(key2)) {
-                list += "- " + key2 + ": " + value2 + "\n" + "}";
+                list += "- " + key2 + ": " + value2 + "\n";
             }
         }
+        String list2 = sort(list);
+        return list2;
+    }
 
-        return list;
+    public static String sort(String list) {
+        List<String> tmp = Arrays.asList(list.split("\n"));
+        ArrayList<String> sortedList = new ArrayList<>();
+        for (int i = 0; i < tmp.size(); i++) {
+            sortedList.add(tmp.get(i));
+            Comparator<String> comparator = (s1, s2) -> {
+                if (s1.length() >= 3 && s2.length() >= 3) {
+                    return s1.substring(2).compareTo(s2.substring(2));
+                }
+                return 0;
+            };
+            sortedList.sort(comparator);
+        }
+        String list3 = String.join("\n", sortedList);
+        String result = "{" + "\n" + list3 + "\n" + "}";
+        return result;
     }
 }
