29636a61996d1eda49ad7488fcb5f688889697b8
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-03-13 23:26:02 +0300

    corrected the code according to the checkstyle

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -10,25 +10,16 @@ import java.util.Map;
 public class Parser {
     public static Map<String, Object> parse(String content, String formatFile) throws IOException {
         ObjectMapper mapper = getMapper(formatFile);
-        Map<String, Object> file = mapper.readValue(content, new TypeReference<>() {
+        return mapper.readValue(content, new TypeReference<>() {
         });
-
-        return file;
     }
 
     public static ObjectMapper getMapper(String formatFile) {
-        ObjectMapper mapper;
-        switch (formatFile) {
-            case ("json"):
-                mapper = new ObjectMapper();
-                break;
-            case ("yaml"):
-            case ("yml"):
-                mapper = new YAMLMapper();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + formatFile);
-        }
+        ObjectMapper mapper = switch (formatFile) {
+            case ("json") -> new ObjectMapper();
+            case ("yaml"), ("yml") -> new YAMLMapper();
+            default -> throw new IllegalStateException("Unexpected value: " + formatFile);
+        };
         return mapper;
     }
 }
